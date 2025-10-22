353d5b05ef59a097091626bf1d66538db2530fe1
Gabriel Oliveira <limagabriel501@gmail.com>
2025-10-22 16:58:53 -0300

    feat: adicionar classe principal Spring Boot e endpoint /api/health; configurar application.properties com H2/JPA

---
+++ src/main/java/br/com/bars_register/BarsRegisterApplication.java
@@ -0,0 +1,11 @@
+package br.com.bars_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarsRegisterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BarsRegisterApplication.class, args);
+    }
+}
