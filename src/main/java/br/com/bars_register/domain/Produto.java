fd65e419e6f1bcd1f0d5e25553f0aacc8dd6c4c3
Gabriel Oliveira <limagabriel501@gmail.com>
2025-10-24 18:09:43 -0300

    Revert "- Implementa soft delete no domínio e JPA"
    This reverts commit a9f294f767293b73a86caab81e60cf31970d4878.

--- src/main/java/br/com/bars_register/domain/Produto.java
+++ src/main/java/br/com/bars_register/domain/Produto.java
@@ -5,7 +5,6 @@ public class Produto {
     private String nome;
     private double preco;
     private int estoque;
-    private boolean ativo = true;
 
     public Produto() {}
 
@@ -14,14 +13,6 @@ public class Produto {
         this.nome = nome;
         this.preco = preco;
         this.estoque = estoque;
-    }
-
-    public Produto(int id, String nome, double preco, int estoque, boolean ativo) {
-        this.id = id;
-        this.nome = nome;
-        this.preco = preco;
-        this.estoque = estoque;
-        this.ativo = ativo;
     }
 
     public int getId() { return id; }
@@ -35,7 +26,4 @@ public class Produto {
 
     public int getEstoque() { return estoque; }
     public void setEstoque(int estoque) { this.estoque = estoque; }
-
-    public boolean isAtivo() { return ativo; }
-    public void setAtivo(boolean ativo) { this.ativo = ativo; }
 }
